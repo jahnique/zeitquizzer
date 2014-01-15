08f54f4fc25c04652f3d19929a7745c5206ebdfa
Yannic Schencking <yannic.schencking@zweitag.de>
2014-01-15 02:33:33 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Zeitquizzer::Application.config.secret_key_base = 'a72d27051d67dc01df5dfb49f0a646c85a066d33c567d9e9e257b79ad9ab6be7a7677cb82243838b45de87f132a617adb6fa4d322b3d36e519dcfad7148e4a49'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Zeitquizzer::Application.config.session_store :cookie_store, key: '_zeitquizzer_session'
